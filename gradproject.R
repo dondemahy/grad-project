c1538ed0d79dde169b40f91e3ae7086709a4dfdb
Don deMahy <don@caterpillar.local>
2019-12-02 14:27:53 -0600

    subsetted NAs and blood taking

--- gradproject.R
+++ gradproject.R
@@ -1,1 +1,15 @@
-test
+library(readr)
+malaria<-read_csv("labo database malaria.csv")
+
+#subsetting out NAs in the Blood column
+NAs2<-malaria[is.na(malaria$Blood),]
+IDs<-unique(malaria$IDBirdBandingData)
+IDs2<-unique(NAs2$IDBirdBandingData)
+t<-IDs %in% IDs2
+t2<-data.frame("IDs"=IDs,"test"=t)
+w<-t2[t2$test==FALSE,]
+w2<-w$IDs
+new.malaria<-malaria[malaria$IDBirdBandingData %in% w2,]
+
+#subsetting out birds for which blood was taken
+blood<-new.malaria[new.malaria$Blood=="Y",]
